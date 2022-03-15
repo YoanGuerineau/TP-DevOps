58406ce23885d0c21e902f9b56d9339e4c013c88
YoanGuerineau <yoan.guerineau@live.fr>
2022-03-15 10:29:36 +0100

    Add IP address output

--- 1/vm-setup.tf
+++ 1/vm-setup.tf
@@ -19,3 +19,10 @@ resource "virtualbox_vm" "node" {
   }
 }
 
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
+
+output "IPAddr_2" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
+}
